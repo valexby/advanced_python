7f3348f6d9da2db0c802070e669c936ece92f718
Alex Vasiuk <alexvasiuk@gmail.com>
2018-09-04 15:52:14 +0300

    Fix linter issues

--- python_killer.py
+++ python_killer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+"""Python killing script"""
 import sys
 
 
 def killme():
+    "Python killing function. Have fun."
     sys.setrecursionlimit(sys.getrecursionlimit() + 1)
     killme()
 
