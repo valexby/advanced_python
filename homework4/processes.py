8a81be46b39bc22d5a74c0ad2de178949c7cb642
Alex Vasiuk <alexvasiuk@gmail.com>
2018-09-18 08:24:23 +0300

    Fix flake8 issues

--- homework4/processes.py
+++ homework4/processes.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""Processes synchronisation example script
-"""
-from multiprocessing import Process
+"""Processes synchronisation example script"""
 from multiprocessing import Pipe
+from multiprocessing import Process
 
 
 def proc(is_eval, connection):
-    """Process body
-    """
+    """Process body"""
     if is_eval:
         print(connection.recv())
     for number in range(int(is_eval), 100, 2):
@@ -17,8 +15,7 @@ def proc(is_eval, connection):
 
 
 def main():
-    """Main function
-    """
+    """Main function"""
     connection1, connection2 = Pipe(duplex=True)
     process1 = Process(target=proc, args=(False, connection1))
     process2 = Process(target=proc, args=(True, connection2))
